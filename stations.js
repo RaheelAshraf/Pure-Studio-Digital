c32d26f6154025983f860f6b96bf9b3228001949
RaheelAshraf <labkoat@gmail.com>
2024-12-27 00:47:02 +0500

    fixed all issues

--- stations.js
+++ stations.js
@@ -1,7 +1,7 @@
 const RADIO_STATIONS = {
-    MAIN_STREAM: 'https://stream.zeno.fm/w8d706c69ehvv',
-    GOSPEL_VIBES: 'https://stream.zeno.fm/w8d706c69ehvv',
-    JAZZ_LOUNGE: 'https://stream.zeno.fm/w8d706c69ehvv',
+    MAIN_STREAM: 'https://s50.radiolize.com/radio/8130/radio.mp3',
+    GOSPEL_VIBES: 'https://stream3.rcast.net/72170',
+    JAZZ_LOUNGE: 'https://stream4.rcast.net/72057',
     CLASSIC_HOUR: 'https://stream.zeno.fm/9yra7d8ederuv'
 };
 
